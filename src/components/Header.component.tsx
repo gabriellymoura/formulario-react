dc3b3858bd4563b8a1c81fc3e0724c4a500f97e4
Glacy <glacy.kelly3@gmail.com>
2022-07-23 20:38:42 -0300

    adicionando styled components

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -3,15 +3,39 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <Container>
-      <span>
-        Formulário para compra de <strong>Pacote de Stickers</strong>
-      </span>
+      <Text>
+        Formulário para compra de <TextBold>Pacote de Stickers</TextBold>
+      </Text>
+      <Image src="src\assets\icon-shape.svg"></Image>
     </Container>
   );
 };
 
 const Container = styled.div`
+  display: flex;
   background: #2f3676;
+  justify-content: space-between;
+  border-bottom-left-radius: 88px;
+  border-bottom-right-radius: 176px;
+  
+`;
+
+const Text = styled.p`
+  color: #fff;
+  font-size: 1.5rem;
+  font-weight: 100;
+  margin: 23px 0 60px 31px;
+  width: 219px;
+  line-height: 35px;
+`;
+
+const TextBold = styled.p`
+  font-weight: bold;
+`;
+
+const Image = styled.img`
+  width: 168px;
+  margin: 7px 50px 0 0;
 `;
 
 export default Header;
