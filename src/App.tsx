0ecd2d652c2b935f2e43aa20e61c19b925b0adbd
Glacy <glacy.kelly3@gmail.com>
2022-07-23 20:42:53 -0300

    adicionando stickers component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.component";
+import Stickers from "./components/Stickers.component"
 
 function App() {
   return (
     <div>
       <Header />
+      <Stickers />
     </div>
   );
 }
---
+++ src/components/Stickers.component.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Stickers = () => {
+    return (
+        <div>
+            <p>Quais stickers?</p>
+        </div>
+    );
+};
+
+export default Stickers;
