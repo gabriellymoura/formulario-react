c1b934bd3e1be439fcd2d938b67975ac7d23a018
Glacy <glacy.kelly3@gmail.com>
2022-07-24 16:10:34 -0300

    estilizando comments component

--- src/components/Comments.component.tsx
+++ src/components/Comments.component.tsx
@@ -2,8 +2,33 @@ import styled from "styled-components";
 
 const Comments = () => {
     return (
-        <div></div>
+        <Container>
+            <Title>Observações</Title>
+            <CommentsArea></CommentsArea>
+        </Container>
     );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+`;
+
+const Title = styled.p`
+  font-weight: bold;
+  font-size: 18px;
+  color: #071723;
+`;
+
+const CommentsArea = styled.textarea`
+    all: unset;
+    border: 1px #2F3676 solid;
+    border-radius: 4px;
+    width: auto;
+    height: 125px;
+    padding: 1rem;
+    background-color: #DDE3E9;
+`;
 
 export default Comments;
