4e80ed1cef7c463aabe924517c6020dc232bba5c
Glacy <glacy.kelly3@gmail.com>
2022-07-24 15:55:13 -0300

    estilizando counter component

--- src/components/Counter.component.tsx
+++ src/components/Counter.component.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 const Counter = () => {
-    return (
-        <Container>
-            <Title>Quantos stickers de cada?</Title>
-        </Container>
-    );
+  return (
+    <Container>
+      <Title>Quantos stickers de cada?</Title>
+      <CounterWrapper>
+        <ButtonCounter>-</ButtonCounter>
+        <DisplayCounter>100</DisplayCounter>
+        <ButtonCounter>+</ButtonCounter>
+      </CounterWrapper>
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -18,6 +23,47 @@ const Title = styled.p`
   color: #071723;
   font-weight: bold;
   margin-bottom: 15px;
+`;
+
+const ButtonCounter = styled.button`
+  all: unset;
+  background-color: #2F3676;
+  color: white;
+  border-radius: 10px;
+  width: 42px;
+  height: 42px;
+  font-size: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  :hover {
+    background-color: #191847;
+  }
+
+  :disabled {
+    background-color: #C5CFD6;
+  }
+`;
+
+const DisplayCounter = styled.div`
+  width: 60px;
+  height: 42px;
+  background-color: #DDE3E9;
+  border: 1px #2F3676 solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: #071723;
+  font-weight: bold;
+  border-radius: 10px;
+  margin: 0 8px;
+`;
+
+const CounterWrapper = styled.div`
+  display: flex;
 `;
 
 export default Counter;
